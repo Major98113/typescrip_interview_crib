57daee9eff41b09c3ba8880126db2c4b800495f5
Maksim <meison98@bk.ru>
2022-10-05 18:23:38 +0300

    feat - Added Selectable ImmutableProps case

---
+++ immutable-props.ts
@@ -0,0 +1,16 @@
+// Напишите generic type ImmutableProps, который добавляет свойство readonly определенным полям переданного тип
+// Пример вызова типа ImmutableProps
+type Product = {
+    id: string
+    categoryId: number
+    name: string
+}
+type ImmutableProps<T, P extends keyof T> = Readonly<Pick<T, P>> & Omit<T, P>;
+
+type ProductWithImmutableIds = ImmutableProps<Product, 'id' | 'categoryId'>
+const product: ProductWithImmutableIds = {id: '1', 'categoryId': 2, name: 'Red carpet'}
+
+// Throws error
+product.id = '1'
+// Ok
+product.name = 'Green carpet'
